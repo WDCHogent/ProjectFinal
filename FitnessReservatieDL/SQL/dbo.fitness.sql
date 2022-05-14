32a6fc8e6ec4022dd5b5bc4fe432396de8e9bdf7
WDCHogent <wout.decrop@student.hogent.be>
2022-05-14 13:40:45 +0200

    v1.2.8.5 - db overhaul -14.05

--- FitnessReservatieDL/SQL/dbo.fitness.sql
+++ FitnessReservatieDL/SQL/dbo.fitness.sql
@@ -1,9 +1,17 @@
+CREATE TABLE [Admin]
+(
+[id] [int] PRIMARY KEY IDENTITY(1,1) NOT NULL,
+[adminnummer]  AS ('A'+right('000'+CONVERT([varchar](10),[ID]),(3))) PERSISTED,
+[naam] [nvarchar](250) NOT NULL,
+[voornaam] [nvarchar](250) NOT NULL
+)
+
 CREATE TABLE [Klant]
 (
 [klantnummer] INT NOT NULL PRIMARY KEY IDENTITY,
 [naam] NVARCHAR(250) NOT NULL,
 [voornaam] NVARCHAR(250) NOT NULL,
-[mailadres] NVARCHAR(250) NOT NULL
+[mailadres] NVARCHAR(250) NOT NULL,
 );
 
 CREATE TABLE [Toesteltype]
@@ -23,8 +31,7 @@ CONSTRAINT [FK_Toestel_Toesteltype] FOREIGN KEY ([toesteltype]) REFERENCES [Toes
 
 CREATE TABLE [Tijdslot]
 (
-[tijdslotid] INT NOT NULL PRIMARY KEY IDENTITY,
-[tijdslot] INT NOT NULL
+[tijdslot] INT PRIMARY KEY NOT NULL,
 );
 
 CREATE TABLE [Reservatie]
@@ -32,14 +39,28 @@ CREATE TABLE [Reservatie]
 [reservatienummer] INT NOT NULL PRIMARY KEY IDENTITY,
 [klantnummer] INT NOT NULL,
 [datum] DATE NOT NULL,
+CONSTRAINT [FK_Reservatie_Klant] FOREIGN KEY ([klantnummer]) REFERENCES [Klant]([klantnummer])
+);
+
+CREATE TABLE [ReservatieInfo]
+(
+[reservatienummer] INT NOT NULL,
 [beginuur] INT NOT NULL,
 [einduur] INT NOT NULL,
 [toestelnummer] INT NOT NULL,
-CONSTRAINT [FK_Reservatie_Klant] FOREIGN KEY ([klantnummer]) REFERENCES [Klant]([klantnummer]),
-CONSTRAINT [FK_Reservatie_Beginuur] FOREIGN KEY (beginuur) REFERENCES [Tijdslot](tijdslotid),
-CONSTRAINT [FK_Reservatie_Einduur] FOREIGN KEY (einduur) REFERENCES [Tijdslot](tijdslotid),
-CONSTRAINT [FK_Reservatie_Toestel] FOREIGN KEY ([toestelnummer]) REFERENCES [Toestel]([toestelnummer])
+CONSTRAINT [FK_ReservatieInfo_Reservatie] FOREIGN KEY ([reservatienummer]) REFERENCES [Reservatie](reservatienummer),
+CONSTRAINT [FK_ReservatieInfo_BeginTijdslot] FOREIGN KEY (beginuur) REFERENCES [Tijdslot]([tijdslot]),
+CONSTRAINT [FK_ReservatieInfo_EindTijdslot] FOREIGN KEY (einduur) REFERENCES [Tijdslot]([tijdslot]),
+CONSTRAINT [FK_ReservatieInfo_Toestel] FOREIGN KEY ([toestelnummer]) REFERENCES [Toestel]([toestelnummer])
 );
 
+DECLARE @tijdslot UNIQUEIDENTIFIER = NEWID();
+DECLARE @mailadres UNIQUEIDENTIFIER = NEWID();
+
+
+
+SELECT r.reservatienummer, r.klantnummer, r.datum, i.beginuur, i.einduur, t.toestelnaam FROM Reservatie r
+LEFT JOIN ReservatieInfo i ON r.reservatienummer=i.reservatienummer
+LEFT JOIN toestel t ON i.toestelnummer=t.toestelnummer
 
 
